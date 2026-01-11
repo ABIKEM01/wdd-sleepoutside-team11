7abd8930163da9efaad0d31fe481bb91ee45d08c
muembiahub <jonathanmuembia3@gmail.com>
2026-01-11 21:57:08 +0200

    indiv

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,21 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // get existing cart or start with empty array
+  let cart = JSON.parse(localStorage.getItem("so-cart"));
+
+  // ensure cart is always an array
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  // add product directly
+  cart.push(product);
+
+  // save back to localStorage
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
