8d414db417e8602aac86c5f74b66b8c9e1d6739b
metalpadawan <emmanuelndem9@gmail.com>
2026-01-21 13:54:13 +0100

    single quotes to js update

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 
 export default class ProductDetails {
   constructor(productId, dataSource) {
@@ -13,14 +13,14 @@ export default class ProductDetails {
     this.renderProductDetails();
 
     document
-      .getElementById("addToCart")
-      .addEventListener("click", this.addProductToCart.bind(this));
+      .getElementById('addToCart')
+      .addEventListener('click', this.addProductToCart.bind(this));
   }
 
   addProductToCart() {
-    const cartItems = getLocalStorage("so-cart") || [];
+    const cartItems = getLocalStorage('so-cart') || [];
     cartItems.push(this.product);
-    setLocalStorage("so-cart", cartItems);
+    setLocalStorage('so-cart', cartItems);
   }
 
   renderProductDetails() {
@@ -29,18 +29,18 @@ export default class ProductDetails {
 }
 
 function productDetailsTemplate(product) {
-  document.querySelector("h2").textContent = product.Brand.Name;
-  document.querySelector("h3").textContent = product.NameWithoutBrand;
+  document.querySelector('h2').textContent = product.Brand.Name;
+  document.querySelector('h3').textContent = product.NameWithoutBrand;
 
-  const productImage = document.getElementById("productImage");
+  const productImage = document.getElementById('productImage');
   productImage.src = product.Image;
   productImage.alt = product.NameWithoutBrand;
 
-  document.getElementById("productPrice").textContent = product.FinalPrice;
-  document.getElementById("productColor").textContent =
+  document.getElementById('productPrice').textContent = product.FinalPrice;
+  document.getElementById('productColor').textContent =
     product.Colors[0].ColorName;
-  document.getElementById("productDesc").innerHTML =
+  document.getElementById('productDesc').innerHTML =
     product.DescriptionHtmlSimple;
 
-  document.getElementById("addToCart").dataset.id = product.Id;
+  document.getElementById('addToCart').dataset.id = product.Id;
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,20 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import { setLocalStorage, getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
+const productId = getParam('product');
 
-const productID = getParam("product");
-
-const product = new ProductDetails(productID, dataSource);
-product.init();
-
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+function addProductToCart(item) {
+  setLocalStorage('so-cart', item);
 }
+
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+async function addToCartHandler() {
+  const selectedProduct = await dataSource.findProductById(productId);
+  addProductToCart(selectedProduct);
 }
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
